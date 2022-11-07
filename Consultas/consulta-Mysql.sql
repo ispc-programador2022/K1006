c7c71969db2d85f09a229231fa4b2f7a11543487
Tomas Escobar <61805924+tomasescobar25@users.noreply.github.com>
2022-11-07 17:05:40 -0300

    Update consulta-Mysql.sql

--- Consultas/consulta-Mysql.sql
+++ Consultas/consulta-Mysql.sql
@@ -1,2 +1,8 @@
+# Top 5 capitalización bursátil
 SELECT * FROM empresas.empresas order by "Capitalización Bursatil" LIMIT 5;
+
+# Cantidad de empresas por país
 SELECT pais, COUNT(*) as cantidad_empresas FROM empresas group by Pais order by cantidad_empresas desc;
+
+#Cantidad de empresas por sector
+SELECT sector, COUNT(*) as cantidad_empresas_sector FROM empresas group by sector order by cantidad_empresas_sector desc;
