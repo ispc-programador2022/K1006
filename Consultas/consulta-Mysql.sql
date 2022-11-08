6aff454f5c516b90a76bb3123a186fad3f365524
fwispc <106827132+fwispc@users.noreply.github.com>
2022-11-08 15:53:01 -0300

    Update consulta-Mysql.sql

--- Consultas/consulta-Mysql.sql
+++ Consultas/consulta-Mysql.sql
@@ -6,3 +6,18 @@ SELECT pais, COUNT(*) as cantidad_empresas FROM empresas group by Pais order by 
 
 #Cantidad de empresas por sector
 SELECT sector, COUNT(*) as cantidad_empresas_sector FROM empresas group by sector order by cantidad_empresas_sector desc;
+
+#Cuáles son los distintos sectores
+SELECT DISTINCT(sector)
+FROM empresas
+ORDER BY sector;
+
+#Empresa con mayor Capitalzación bursátil por sector
+WITH empresas AS (
+  SELECT *,
+    row_number() OVER (PARTITION BY sector ORDER BY cap_bursatil) as row_num
+  FROM empresas
+)
+SELECT *
+FROM empresas
+WHERE row_num = 1;
